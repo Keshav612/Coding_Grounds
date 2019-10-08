800ea3f350c659e40b87850ac0de3390e37411c1
Keshav612 <500062098@stu.upes.ac.in>
2019-10-08 19:35:04 +0530

    Hello World challenge

---
+++ 30DaysOfCode/HelloWorld_D1.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class HelloWorld_D1 {
+	public static void main(String[] args) {    
+		Scanner scan = new Scanner(System.in); 	
+		scan.close();		
+		System.out.println("Hello, World.");
+   
+	}
+}
+
