872b90f56883f566198d77ab69fbfc24cf0eed19
Keshav612 <500062098@stu.upes.ac.in>
2019-10-09 20:31:18 +0530

    Day11 challenge completed

---
+++ 30DaysOfCode/Arrays2D.java
@@ -0,0 +1,53 @@
+//https://www.hackerrank.com/challenges/30-2d-arrays/problem
+//Keshav Mishra
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Arrays2D {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        int[] s=new int[16];
+        int c=0;
+        for(int i=0;i<4;i++) 
+        {
+            for(int j=0;j<4;j++) 
+            {
+                s[c]=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                c++;
+            }
+        }
+        Arrays.sort(s); //This function is not usable in eclipse IDE.Run the program in Hackerrank terminal..
+        System.out.print(s[15]);
+        scanner.close();
+    }
+}
+
+
