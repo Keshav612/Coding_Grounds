45aad85cfc11db9bd708307ff5ae8adc064803b3
Keshav612 <500062098@stu.upes.ac.in>
2020-12-05 11:05:45 +0530

    Celcius to Fahreheit

---
+++ PPC/CtoF.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+class CtoF 
+{
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int T=sc.nextInt();//input number of testcases
+		while(T-->0)
+		{
+		    CelsiusToFahrenheit obj=new CelsiusToFahrenheit();
+		    
+		    int C;
+		    C=sc.nextInt();//input temperature in celscius
+		    
+		    System.out.println((int)(obj.cToF(C)));//print the output
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class CelsiusToFahrenheit
+{
+    public double cToF(int C)
+    {
+        return (C*9/5) + 32;
+    }
+}
+
