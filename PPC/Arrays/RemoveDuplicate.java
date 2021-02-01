bf9b78d8952c3092964b2f9a31143bd6930d0a06
Keshav612 <500062098@stu.upes.ac.in>
2021-02-01 23:57:33 +0530

    Updated Readme file

---
+++ PPC/Arrays/RemoveDuplicate.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class RemoveDuplicate {
+    
+    public static void main(String[] args) {
+        int a[]={1,2,2,2,2};
+        int n=a.length;
+
+    }
+    int remove(int a[],int n)
+    {
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+            if(a[i]==a[j])
+            {
+                
+            }
+            }
+        }
+        return -1;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JAVA
 This repository contains solution of the challenges available on various coding ground like hackerrank and others.These solutions are available in JAVA language.
+Also contains solution to multiple questions.
